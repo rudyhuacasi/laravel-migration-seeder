91a8c9cb41f5f857af958b9faa4e3dce2e482fb6
rudyhuacasi <alexisitaliano16@gmail.com>
2024-07-15 07:29:57 +0200

    {UPD} added  biglietti treni

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,40 @@
 </head>
 
 <body>
-    <h1>laravel template</h1>
+    <header class="bg-info mb-5">
+        <div class="container text-center bg-info p-3">
+            <H1><em>biglietti per i treni</em></H1>
+        </div>
+    </header>
 
-    <img src="{{ Vite::asset('resources/img/kaneki.jpg') }}" alt="paperella lavarel template">
+    <main>
+        <div class="container text-center">
+            <div class="row row-gap-3">
+                @foreach($treni as $treno)
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-body height">
+                            <h3 class="card-title pb-3">{{$treno['azienda']}}</h3>
+                            <p><strong>STAZIONE DI PARTENZA:</strong> {{$treno['stazione_di_partenza']}}</p>
+                            <p><strong>STAZIONE DI ARRIVO:</strong> {{$treno['stazione_di_arrivo']}}</p>
+                            <p><strong>ORARIO DI PARTENZA:</strong> {{$treno['orario_di_partenza']}}</p>
+                            <p><strong>ORARIO DI ARRIVO:</strong> {{$treno['orario_di_arrivo']}}</p>
+                            <p><strong>CODICE TRENO:</strong> {{$treno['codice_treno']}}</p>
+                            <p><strong>NUMERO CARROZZE:</strong> {{$treno['numero_carrozze']}}</p>
+                            @if ($treno['in_orario'] === 1)
+                            <p><strong>in orario</strong></p>
+                            @elseif ($treno['cancellato'] === 1)
+                            <p><strong>cancellato</strong> </p>
+                            @endif
+
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </main>
+    
 </body>
 
 </html>
